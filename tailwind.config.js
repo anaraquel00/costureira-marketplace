b99227e404e7aa4942e7d5fdd93bfae5f2037afc
Ana Raquel de Holanda <anaraquel00@users.noreply.github.com>
2025-05-23 00:38:57 +0000

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        primary: '#D94790',
+        secondary: '#9333EA',
+        light: '#F5F5F5'
+      }
+    },
+  },
+  plugins: [],
+}
